dcb84a83cc5b71f7cce1084592d58b687ec8acdd
qianang.feng <qianang.feng@wenba100.com>
2017-10-10 16:07:55 +0800

    update something

--- app/src/main/java/com/wenba/aixuestore/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/wenba/aixuestore/data/source/remote/RemoteDataSource.kt
@@ -1,41 +1,29 @@
 package com.wenba.aixuestore.data.source.remote
 
+import com.google.gson.reflect.TypeToken
 import com.wenba.aixuestore.data.source.AppDataSource
 import com.wenba.aixuestore.data.source.BaseAppInfo
 import com.wenba.aixuestore.network.BaseResponse
 import com.wenba.aixuestore.network.OkHttpKotlinHelper
 import com.wenba.aixuestore.util.JsonWrapper
 import com.wenba.aixuestore.util.UrlMapping
-import com.google.gson.reflect.TypeToken
 import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import okhttp3.FormBody
-import okhttp3.Request
 import org.json.JSONObject
 
 
 class RemoteDataSource : AppDataSource {
 
-    val tag = "RemoteDataSource"
-
     override fun loadAppDetail(aKey: String, _api_key: String): Flowable<JSONObject> {
         return Flowable.just(UrlMapping.APP_DETAIL)
                 .subscribeOn(Schedulers.io())
                 .flatMap({ url ->
-                    val formBody = FormBody.Builder()
-                            .add("aKey", aKey)
-                            .add("_api_key", _api_key)
-                            .build()
-                    var request = Request.Builder()
-                            .url(url)
-                            .post(formBody)
-                            .build()
-                    val response = OkHttpKotlinHelper.getOkHttpClient().newCall(request).execute()
-                    Flowable.just(response)
-                })
-                .flatMap({ response ->
-                    val result = response.body()!!.string()
+                    val map = HashMap<String, Any>()
+                    map.put("aKey", aKey)
+                    map.put("_api_key", _api_key)
+                    val response = OkHttpKotlinHelper.postFormSync(url, map)
+                    val result = response.body()?.string()
                     Flowable.just(JSONObject(result))
                 })
                 .observeOn(AndroidSchedulers.mainThread())
@@ -46,19 +34,11 @@ class RemoteDataSource : AppDataSource {
         return Flowable.just(UrlMapping.ListMyPublished)
                 .subscribeOn(Schedulers.io())
                 .flatMap({ url ->
-                    val formBody = FormBody.Builder()
-                            .add("uKey", ukey)
-                            .add("_api_key", _api_key)
-                            .build()
-                    var request = Request.Builder()
-                            .url(url)
-                            .post(formBody)
-                            .build()
-                    val response = OkHttpKotlinHelper.getOkHttpClient().newCall(request).execute()
-                    Flowable.just(response)
-                })
-                .flatMap({ response ->
-                    val result = response.body()!!.string()
+                    val map = HashMap<String, Any>()
+                    map.put("uKey", ukey)
+                    map.put("_api_key", _api_key)
+                    val response = OkHttpKotlinHelper.postFormSync(url, map)
+                    val result = response.body()?.string()
                     Flowable.just(JsonWrapper.parse<BaseResponse<BaseAppInfo>>(result, object : TypeToken<BaseResponse<BaseAppInfo>>() {}.type))
                 })
                 .observeOn(AndroidSchedulers.mainThread())
--- app/src/main/java/com/wenba/aixuestore/network/OkHttpKotlinHelper.kt
+++ app/src/main/java/com/wenba/aixuestore/network/OkHttpKotlinHelper.kt
@@ -1,16 +1,44 @@
 package com.wenba.aixuestore.network
 
-import okhttp3.OkHttpClient
+import android.util.Log
+import okhttp3.*
 
 object OkHttpKotlinHelper {
+
+    private val tag = "OkHttpKotlinHelper"
 
     private var client: OkHttpClient? = null
 
     @JvmStatic
     fun getOkHttpClient(): OkHttpClient {
         if (client == null) {
-            client = OkHttpClient()
+            client = OkHttpClient.Builder()
+                    .addInterceptor(LogInterceptor()).build()
         }
         return client!!
+    }
+
+    class LogInterceptor : Interceptor {
+        override fun intercept(chain: Interceptor.Chain?): Response {
+            val request = chain?.request()
+            val response = chain?.proceed(request)
+            Log.d(tag, "request->" + request.toString())
+            Log.d(tag, "response->" + response.toString())
+            return response!!
+        }
+    }
+
+    @JvmStatic
+    fun postFormSync(url: String, params: Map<String, Any>): Response {
+        val keys = params.keys
+        val formBody = FormBody.Builder()
+        for (key in keys) {
+            formBody.add(key, params[key] as String?)
+        }
+        var request = Request.Builder()
+                .url(url)
+                .post(formBody.build())
+                .build()
+        return OkHttpKotlinHelper.getOkHttpClient().newCall(request).execute()
     }
 }
